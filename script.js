ffb100bdbeee799849abe02bbd8d13ef06fdd78b
unknown <costalucas2004@gmail.com>
2023-11-03 09:05:24 -0300

    :tada: initial comit

---
+++ script.js
@@ -0,0 +1,23 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  if (html.classList.contains("light")) {
+  html.classList.remove("light")
+  } else {
+   html.classList.add("light")
+  }
+//pegar a tag img
+  const img = document.querySelector("#profile img")
+//substituir a imagem
+  if(html.classList.contains('light')){
+   img.setAttribute('src', './assets/avatar-light.png')
+  } else{
+img.setAttribute('src', './assets/Avatar.png')
+  }
+  //html.classList.toggle("light")
+
+
+
+
+
+}
